d3dc7e8611ff377bae5fe8a5d816905dbcf8da98
Danny Cahoon <daniel.cahoon@gmail.com>
2018-01-27 16:00:40 -0700

    player controlled mouse looking and death mechanic

--- Assets/Code/testPlayerController.cs
+++ Assets/Code/testPlayerController.cs
@@ -1,35 +1,85 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class testPlayerController : MonoBehaviour {
+	// Mouse Look
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+	float rotationX = 0F;
+	float rotationY = 0F;
+	Quaternion originalRotation;
 
+	// Health/Death
+	public int health;
+	public string mainMenu;
 	// Use this for initialization
 	void Start () {
-		
+		originalRotation = transform.localRotation;
 	}
 	
 	// Update is called once per frame
 	void Update()
     {
-        if (Input.GetMouseButton(1)) //strafing
-        {
-            Cursor.visible = false;
-            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f * this.transform.lossyScale.magnitude;
-            var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f * this.transform.lossyScale.magnitude;
+		deathCheck ();
+		Cursor.visible = false;
+		mouseLook ();
+        Cursor.visible = false;
 
-            transform.Translate(x, 0, 0);
-            transform.Translate(0, 0, z);
-        }
-        else
-        {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f * this.transform.lossyScale.magnitude;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f * this.transform.lossyScale.magnitude;
 
-            Cursor.visible = true;
-            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-            var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f * this.transform.lossyScale.magnitude;
+        transform.Translate(x, 0, 0);
+        transform.Translate(0, 0, z);
+	}
 
-            transform.Rotate(0, x, 0);
-            transform.Translate(0, 0, z);
-        }
-    }
+	void mouseLook() {
+		Cursor.lockState = CursorLockMode.Locked;
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			// Read the mouse input axis
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationX = ClampAngle (rotationX, minimumX, maximumX);
+			rotationY = ClampAngle (rotationY, minimumY, maximumY);
+			Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+			Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, -Vector3.right);
+			transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+			rotationX = ClampAngle (rotationX, minimumX, maximumX);
+			Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+			transform.localRotation = originalRotation * xQuaternion;
+		}
+		else
+		{
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = ClampAngle (rotationY, minimumY, maximumY);
+			Quaternion yQuaternion = Quaternion.AngleAxis (-rotationY, Vector3.right);
+			transform.localRotation = originalRotation * yQuaternion;
+		}
+	}
+	public static float ClampAngle (float angle, float min, float max)
+	{
+		if (angle < -360F)
+			angle += 360F;
+		if (angle > 360F)
+			angle -= 360F;
+		return Mathf.Clamp (angle, min, max);
+	}
+
+	public void deathCheck() {
+		if (health <= 0) { 
+			SceneManager.LoadScene(mainMenu);
+		}
+	}
 }
