e4cd404ba877d3d97acad7d357f26d624d3e91a2
Nate B <bryantnathanj@gmail.com>
2018-01-27 02:13:08 -0700

    1) testPlayerControls.cs - WSAD movement 2) billboard.cs - view oriented billboard code

---
+++ Assets/Code/billboard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class billboard : MonoBehaviour {
+
+    public float maxDistance = 10.0f;
+    public float minDistance = 2.0f;
+    public float scaleFactor = 0.03f; //edit this to control size on screen
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // face sprite at camera
+        transform.LookAt(Camera.main.transform.position, -Vector3.up);
+
+        // show/hide billboard based on distance
+        var distance = Vector3.Distance(Camera.main.transform.position, this.transform.position);
+        if (distance < maxDistance && distance > minDistance)
+        {
+            GetComponent<Renderer>().enabled = true;
+        } else
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+
+        //scale billboard to fixed screen size
+        float size = (Camera.main.transform.position - transform.position).magnitude;
+        transform.localScale = new Vector3(size, size, size) * scaleFactor;
+    }
+}
